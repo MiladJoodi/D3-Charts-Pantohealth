4235c20be7f05aebe5649148aaea5c305fa44bb3
Joodi <winbeta.com@gmail.com>
2025-07-31 15:11:28 +0330

    up

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import { ChartPage } from "./components/ChartPage";
 
 export default function Home() {
   return (
-    <main className="p-8">
+    <main className="flex flex-col justify-content-center items-center p-8">
       <ChartPage />
     </main>
   )
